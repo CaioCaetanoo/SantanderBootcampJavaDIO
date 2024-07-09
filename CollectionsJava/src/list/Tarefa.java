2f0a2cd5845173bf097c5fad0df25ff80765b0d7
CaioCaetano <caio_c_aetano@outlook.com>
2024-07-09 17:28:21 -0300

    Resolvendo lista tarefa
    Signed-off-by: CaioCaetano <caio_c_aetano@outlook.com>

---
+++ CollectionsJava/src/list/Tarefa.java
@@ -0,0 +1,19 @@
+package list;
+
+public class Tarefa {
+    //atributo
+        private String descricao;
+
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    @Override
+    public String toString() { //usado para mostrar a descrição do item, e nao o endereço na memória.
+        return  descricao;
+    }
+}
