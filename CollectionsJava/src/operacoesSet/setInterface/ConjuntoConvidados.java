c02ffc13fea1fa9ee3ec17988dcf399d9a7a0225
CaioCaetano <caio_c_aetano@outlook.com>
2024-07-09 19:53:15 -0300

    operações basicas set

---
+++ CollectionsJava/src/operacoesSet/setInterface/ConjuntoConvidados.java
@@ -0,0 +1,51 @@
+package operacoesSet.setInterface;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ConjuntoConvidados {
+
+    private Set<Convidado> convidadoSet;
+
+    public ConjuntoConvidados() {
+        this.convidadoSet = new HashSet<>();
+    }
+
+    public void adicionarConvidado(String nome, int codigoConvite) {
+        convidadoSet.add(new Convidado(nome, codigoConvite));
+    }
+
+    public void removerConvidadoPorCodigoConvite(String codigoConvite) {
+        Convidado convidadoParaRemover = null;
+        for(Convidado c : convidadoSet){
+            if(c.getCodigoConvite() == codigoConvite) {
+                convidadoParaRemover = c;
+                break;
+            }
+        }
+        convidadoSet.remove(convidadoParaRemover);
+    }
+
+    public int contarConvidados() {
+        return convidadoSet.size();
+    }
+
+    public void exibirConvidados() {
+        System.out.println(convidadoSet);
+    }
+
+    public static void main(String[] args) {
+        ConjuntoConvidados conjuntoConvidados = new ConjuntoConvidados();
+        conjuntoConvidados.adicionarConvidado("Caio", 1234);
+        conjuntoConvidados.adicionarConvidado("Pedro", 1235);
+        conjuntoConvidados.adicionarConvidado("João", 1236);
+        conjuntoConvidados.adicionarConvidado("Lucas", 1236);
+
+        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + "dentro do Set de convidados");
+
+        conjuntoConvidados.removerConvidadoPorCodigoConvite(1234);
+        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + "dentro do Set de convidados");
+
+        conjuntoConvidados.exibirConvidados();
+    }
+}
