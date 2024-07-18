6911f7c120c82201ba7aab26596d10020a3b0c79
CaioCaetano <caio_c_aetano@outlook.com>
2024-07-17 21:54:57 -0300

    Desafio "Criando um banco digital com java e OO".

---
+++ lab-banco-digital-oo-master/src/criacaoContas/ContaCorrente.java
@@ -0,0 +1,17 @@
+package criacaoContas;
+
+import clientes.Cliente;
+
+public class ContaCorrente extends Conta {
+
+	public ContaCorrente(Cliente cliente) {
+		super(cliente);
+	}
+
+	@Override
+	public void imprimirExtrato() {
+		System.out.println("=== Extrato contaInfos.Conta Corrente ===");
+		super.imprimirInfosComuns();
+	}
+	
+}
